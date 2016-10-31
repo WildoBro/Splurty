425d38bd12cad36a4087b59c2fdd27c7dc2f643b
Willie Broder <willie.broder@gmail.com>
2016-10-31 21:16:27 +0000

    Add the html hello message

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+# this line is added by instruction
+  root 'quotes#index'
+# this was the added line
 end
